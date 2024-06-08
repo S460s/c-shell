8d0850f8199372f1c944e598f9991ca318bcf03c
s460 <66819532+S460s@users.noreply.github.com>
2024-06-08 22:10:52 +0300

    fix errors

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -1,4 +1,5 @@
 #include "builtins.h"
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,72 +9,99 @@
 #include <unistd.h>
 #include "../util/util.h"
 
-ssize_t execute_exit(char* input){
-      char* arguments = input + 4;
-      errno = 0;
-      char* endptr;
-      int exit_status = strtol(arguments, &endptr, 10);
+ssize_t execute_exit(char *input)
+{
+  char *arguments = input + 4;
+  errno = 0;
+  char *endptr;
+  int exit_status = strtol(arguments, &endptr, 10);
 
-
-      if(errno != 0){
-        perror("strtol");
-        exit(EXIT_FAILURE);
-      }
-
-      if(endptr == arguments){
-        fprintf(stderr, "No exit status was found\n");
-        return -1;
-      }
-
-      exit(exit_status);
+  if (errno != 0)
+  {
+    perror("strtol");
+    exit(EXIT_FAILURE);
   }
 
-ssize_t execute_echo(char* input){
-    printf("%s\n", input + 5);
-    return 0;
-  }
-
-ssize_t execute_type(char* input){
-    char builtins[][16] = {"echo", "type", "exit"};
-    char* arguments = input + 5;
-
-    for(size_t i = 0; i < sizeof(builtins) / 16; i++){
-      if(strcmp(builtins[i], arguments) == 0){
-        printf("%s is a shell builtin\n", arguments);
-        return 0;
-      }
-    }
-
-    char* result = command_path(arguments);
-    if(result != NULL){
-      printf("%s is %s\n", arguments, result);
-      free(result);
-      return 0;
-    }
-
-    printf("%s: not found\n", arguments);
+  if (endptr == arguments)
+  {
+    fprintf(stderr, "No exit status was found\n");
     return -1;
   }
 
-struct BuiltIn** create_builtins(size_t* size){
-  struct BuiltIn* _echo = malloc(sizeof(struct BuiltIn));
+  exit(exit_status);
+}
+
+ssize_t execute_echo(char *input)
+{
+  printf("%s\n", input + 5);
+  return 0;
+}
+
+ssize_t execute_type(char *input)
+{
+  char builtins[][16] = {"echo", "type", "exit"};
+  char *arguments = input + 5;
+
+  for (size_t i = 0; i < sizeof(builtins) / 16; i++)
+  {
+    if (strcmp(builtins[i], arguments) == 0)
+    {
+      printf("%s is a shell builtin\n", arguments);
+      return 0;
+    }
+  }
+
+  char *result = command_path(arguments);
+  if (result != NULL)
+  {
+    printf("%s is %s\n", arguments, result);
+    free(result);
+    return 0;
+  }
+
+  printf("%s: not found\n", arguments);
+  return -1;
+}
+
+ssize_t execute_pwd(char *input)
+{
+  char pwd[PATH_MAX];
+  char *pathname = getcwd(pwd, PATH_MAX);
+
+  if (pathname == NULL)
+  {
+    fprintf(stderr, "getcwd error\n");
+    exit(1);
+  }
+
+  printf("%s\n", pwd);
+  return 0;
+}
+
+struct BuiltIn **create_builtins(size_t *size)
+{
+  struct BuiltIn *_echo = malloc(sizeof(struct BuiltIn));
   strcpy(_echo->command, "echo");
   _echo->execute = execute_echo;
 
-  struct BuiltIn* _exit = malloc(sizeof(struct BuiltIn));
+  struct BuiltIn *_exit = malloc(sizeof(struct BuiltIn));
   strcpy(_exit->command, "exit");
   _exit->execute = execute_exit;
 
-  struct BuiltIn* _type = malloc(sizeof(struct BuiltIn));
+  struct BuiltIn *_type = malloc(sizeof(struct BuiltIn));
   strcpy(_type->command, "type");
   _type->execute = execute_type;
 
+  struct BuiltIn *_pwd = malloc(sizeof(struct BuiltIn));
+  strcpy(_pwd->command, "pwd");
+  _pwd->execute = execute_pwd;
 
-  *size = 3;
-  struct BuiltIn** builtins = malloc(sizeof(struct BuiltIn*) * (*size));
+  *size = 4;
+  struct BuiltIn **builtins = malloc(sizeof(struct BuiltIn *) * (*size));
   builtins[0] = _echo;
   builtins[1] = _exit;
   builtins[2] = _type;
+  builtins[3] = _pwd;
 
   return builtins;
 }
--- src/main.c
+++ src/main.c
@@ -11,65 +11,79 @@
 #include "builtins/builtins.h"
 #include "util/util.h"
 
-void handle_input(char* input){
-    size_t size = 0;
-    struct BuiltIn** builtins = create_builtins(&size);     
-    for(size_t i = 0; i < size; i++){
-      struct BuiltIn* current = builtins[i];
-      if(strncmp(current->command, input, strlen(current->command)) == 0){
-        current->execute(input);
-        return;
-      }
+void handle_input(char *input)
+{
+  size_t size = 0;
+  struct BuiltIn **builtins = create_builtins(&size);
+  for (size_t i = 0; i < size; i++)
+  {
+    struct BuiltIn *current = builtins[i];
+    if (strncmp(current->command, input, strlen(current->command)) == 0)
+    {
+      current->execute(input);
+      return;
     }
+  }
 
-  char* copy_input = malloc(strlen(input));
+  char *copy_input = malloc(strlen(input));
   strcpy(copy_input, input);
 
-  char* program_name = strtok(copy_input, " ");
-  char* program = command_path(program_name);
+  char *program_name = strtok(copy_input, " ");
+  char *program = command_path(program_name);
 
-  if(program){
+  if (program)
+  {
     // args should hold pointers to chunks of "strtok"ed string so freeing the string directly should be fine
-    char** args = malloc(sizeof(char*) * 16);
-    char* copy_input = malloc(strlen(input));
+    char **args = malloc(sizeof(char *) * 16);
+    char *copy_input = malloc(strlen(input));
     strcpy(copy_input, input);
-    char* program_name = strtok(copy_input, " ");
+    char *program_name = strtok(copy_input, " ");
     args[0] = program_name;
 
-    char* arg;
+    char *arg;
     size_t i;
-    for(i = 1; (arg = strtok(NULL, " ")) != NULL; i++){
+    for (i = 1; (arg = strtok(NULL, " ")) != NULL; i++)
+    {
       args[i] = arg;
     }
     args[i] = NULL;
 
     pid_t pid = fork();
 
-    switch (pid) {
-      case -1:
-        perror("fork");
-        exit(EXIT_FAILURE);
-      case 0:
-        execv(program, args);
-        exit(EXIT_SUCCESS);
+    switch (pid)
+    {
+    case -1:
+      perror("fork");
+      exit(EXIT_FAILURE);
+    case 0:
+      execv(program, args);
+      exit(EXIT_SUCCESS);
     }
 
-    while(1){
-    int status;
-    pid_t done = wait(&status);
-    if(done == -1){
-      if(errno == ECHILD) break;
-    }else {
-      if(!WIFEXITED(status) || WEXITSTATUS(status) != 0){ // 
-        fprintf(stderr, "Pid %d failed\n", done);
-        exit(1);
+    while (1)
+    {
+      int status;
+      pid_t done = wait(&status);
+      if (done == -1)
+      {
+        if (errno == ECHILD)
+          break;
+      }
+      else
+      {
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
+        { //
+          fprintf(stderr, "Pid %d failed\n", done);
+          exit(1);
         }
       }
     }
     free(copy_input);
     free(args);
-  }else{
-    fprintf(stderr ,"%s: command not found\n", input);
+  }
+  else
+  {
+    fprintf(stderr, "%s: command not found\n", input);
   }
 
   free(copy_input);
@@ -78,10 +92,11 @@ void handle_input(char* input){
 
 int main()
 {
-  while (1) {
+  while (1)
+  {
     printf("$ ");
     fflush(stdout);
-    char* input = read_line();
+    char *input = read_line();
     handle_input(input);
     free(input);
   }
